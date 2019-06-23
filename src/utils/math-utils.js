570fcbd50e4a265e3f92299784da1178bf27c20f
Jakub Stebel <k.stebel@seznam.cz>
2019-06-23 15:47:48 +0200

    Feat getStdDev
    Implement getStdDev function
    Test getStdDev function

--- src/utils/math-utils.js
+++ src/utils/math-utils.js
@@ -9,3 +9,19 @@ export const getMean = (values) => {
 
     return (sum / values.length);
 };
+
+/**
+ * Compute standard deviation of the whole population
+ * @param {number[]} values 
+ * @returns {number}
+ */
+export const getStdDev = (values) => {
+    /** @type {number} */
+    const valuesMean = getMean(values);
+    /** @type {number[]} */
+    const meanSquaredDiffs = values.map((value) => Math.pow((value - valuesMean), 2));
+    /** @type {number} */
+    const diffsMean = getMean(meanSquaredDiffs);
+
+    return Math.sqrt(diffsMean);
+};
